cc39cda3bed27cc9a199d4f65ab83c1debc7538f
YouKnowWhoIs <leokatmax@gmail.com>
2024-01-27 19:26:18 +0200

    up

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,6 +3,28 @@ const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (email === '' || message === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  const data = {
+    email,
+    message,
+  };
+
+  console.log(data);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 
 function onFormInput() {
   const email = form.elements.email.value;
@@ -34,8 +56,8 @@ function loadFromLS(key) {
 function init() {
   const data = loadFromLS(STORAGE_KEY) || {};
 
-  form.elements.email.value = data.email;
-  form.elements.message.value = data.message;
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
 }
 
 init();
